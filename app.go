97b59c86a9008764a1667eda1f286dd1946ed475
TodorovManuel <101262896+TodorovManuel@users.noreply.github.com>
2024-05-19 23:57:59 -0300

    Add files via upload

---
+++ app.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func Index(rw http.ResponseWriter, r *http.Request) {
+	template, _ := template.ParseFiles("templates/index.html")
+	template.Execute(rw, nil)
+}
+
+func main() {
+	http.HandleFunc("/", Index)
+
+	//Creación del servidor
+	fmt.Println("El servidor esta corriedno en el puerto 3000")
+	http.ListenAndServe("localhost:3000", nil)
+}
